ec90831fa7a33cbbdb658aa9504da7ced5f6d052
Ariel Jannai <arieljannai@gmail.com>
2017-09-16 11:37:37 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const Highlighter = require('./lib/highlighter');
+const wbs = require('./lib/webshot');
+
+const codeData = 'function yo(thing) {\n' +
+	'    console.log(thing);\n' +
+	'};\n';
+
+let hl = new Highlighter(codeData);
+
+main();
+
+function main() {
+	hl.createHtml()
+		.then((html) => {
+			return wbs.webshot(html);
+		})
+		.then((content) => {
+			console.log(content);
+		})
+		.then(() => console.log('done'))
+		.catch((err) => console.error('oh no', err));
+}
---
+++ settings.js
@@ -0,0 +1,21 @@
+module.exports = {
+	htmlTemplate: {
+		content: '<head>__INSERT_STYLE_HERE</head><body>' +
+			'<pre class="hljs"><code>__INSERT_CODE_HERE</code></pre></body>',
+		styleReplacement: '__INSERT_STYLE_HERE',
+		codeReplacement: '__INSERT_CODE_HERE'
+	},
+	styleTemplate: {
+		content: 'node_modules/highlight.js/styles/__STYLE_NAME.css',
+		nameReplacement: '__STYLE_NAME'
+	},
+	defaults: {
+		style: 'monokai'
+	},
+	names: {
+		webshot: {
+			tmpPostfix: '.png',
+			tmpPrefix: 'hljs-webshot-'
+		}
+	}
+};
